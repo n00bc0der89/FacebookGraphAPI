8ce05f6c6b0d7a24d450e10d3fb78fe4c391bc37
Sachin Shetty <scn.3089@gmail.com>
2016-12-07 01:52:20 +0530

    Update facebookgraphapi.js

--- facebookgraphapi.js
+++ facebookgraphapi.js
@@ -2,13 +2,9 @@ var graph = require('fbgraph');
 const config = require('./config');
 const constants = config.constants;
 const mysql      = require('mysql');
-const kafka  = require('kafka-node');
-var Producer 		= kafka.Producer;
-var kafkaClient 	= new kafka.Client('localhost:2181');
-var producer 		= new Producer(kafkaClient);
-
-
- 
+var fs = require('fs');
+var jsonparsing = require('./jsonparsing');
+var completedata = ""; 
  graph.setAccessToken(constants.access_token);
  
  const connection = mysql.createConnection({
@@ -21,7 +17,7 @@ var producer 		= new Producer(kafkaClient);
  connection.connect();
  graph.setVersion("2.8");
  
- 	payloads = [{	 topic: 'facebooktopic', messages: "", partition: 0 }];
+ 
  
  connection.query("SELECT * FROM restaurantlist",function(ferr,frows,ffields)
     {
@@ -36,6 +32,8 @@ var producer 		= new Producer(kafkaClient);
                     var ep = frows[n].epoch;
                     var date = new Date().toISOString();
                     var params = "";
+                    completedata = "";
+                    
                     if(ep == null)
                     {
                       params = { fields: "id,about,bio,business,category,category_list,cover,description,engagement,fan_count,general_info,hours,is_always_open,is_verified,is_permanently_closed,is_unclaimed,link,location,name,overall_star_rating,place_type,price_range,rating_count,username,verification_status,website,feed.until("+ date +"){message,place,link,actions,message_tags,scheduled_publish_time}" }; 
@@ -55,14 +53,9 @@ var producer 		= new Producer(kafkaClient);
                       if(res != null)
                       {
                           var data = res;
-                      
-                              //console.log(data);
-                         payloads.messages = JSON.stringify(data);
+                         completedata = data;
                          
-                         producer.send(payloads, function (err, data) {
-						    console.log('Pushed Successfully');
-					    });
-                      }
+                      } 
                       
                       if(data.feed != undefined)
                       {
@@ -72,6 +65,9 @@ var producer 		= new Producer(kafkaClient);
                       }
                       else
                       {
+                          //For Pages which does not have feeds spanned across pages.
+                          jsonparsing.getParsedString(JSON.stringify(completedata));
+    			      
                           getDetails(n + 1);
                       }
                     });
@@ -85,6 +81,7 @@ var producer 		= new Producer(kafkaClient);
             
         function recursivecall(res,n)
           {
+              
               if(res.paging && res.paging.next)
               {
                   graph.get(res.paging.next, function(nerr, nres) {
@@ -94,19 +91,19 @@ var producer 		= new Producer(kafkaClient);
                       }
                         var ndata = nres;
           
-                             // console.log(ndata);
-                            payloads.messages = JSON.stringify(data);
-                         
-                             producer.send(payloads, function (err, data) {
-    						    console.log('Pushed Successfully');
-    					    });
-                           
-                           
+                         for(var d = 0; d < ndata.data.length; d++) //Push all paginated feed data into single feed data array
+                         {
+                           completedata.feed.data.push(ndata.data[d]);
+                         }
+                          
                           recursivecall(nres,n); //Recursive call
                   });
               }
               else
               {
+    			   jsonparsing.getParsedString(JSON.stringify(completedata));
+    			      
+    			
                   //Update sql table : epoch field with todays date time.
                   connection.query('UPDATE restaurantlist SET epoch = "'+ new Date().toISOString() +'" WHERE restaurantid = "'+ frows[n].restaurantid +'";', function(err, rows, fields) {
 					if (err) 
@@ -115,7 +112,7 @@ var producer 		= new Producer(kafkaClient);
 					}
 					else
 					{
-					    console.log("Updated");
+					    //console.log("Updated");
 					}
 				});
                   
