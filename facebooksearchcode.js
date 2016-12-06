0b5266c54439055b4b1afe71653d5777670477a7
Sachin Shetty <scn.3089@gmail.com>
2016-12-06 19:45:45 +0530

    updated

--- facebooksearchcode.js
+++ facebooksearchcode.js
@@ -8,7 +8,7 @@ const constants = config.constants;
  graph.setAccessToken(constants.access_token);
   graph.setVersion("2.8");
   
-var searchFor = { q: 'restaurants',type: 'place', center: '51.50,-0.10', distance: 10000 };
+var searchFor = { q: 'restaurant',type: 'place', center: '51.50,-0.10', distance: 100000, limit:500 };
 
 console.log("host: " + process.env.IP);
 console.log("port: " + process.env.PORT);
@@ -21,7 +21,8 @@ const connection = mysql.createConnection({
 });
  connection.connect();
 
-
+function getRestaurantNames(searchFor)
+{
 graph.search(searchFor, function(err, res) { 
     if(err)
     {
@@ -46,69 +47,48 @@ graph.search(searchFor, function(err, res) {
                     			
                     			console.log("Row inserted");
                     			 getFbList(i + 1);
+                    			 
                     		});
+                       }
+                       else if(frows.length == 1)
+                       {
+                           getFbList(i + 1);
                        }
                    });
                    
                
            
         }
-        
-            if(res.paging && res.paging.next)
-           {
-               recursivesearchcall(res);
-           }
-    }
+        else
+        {
+            if(res.paging != undefined)
+            {
+                if(res.paging.cursors.after)
+               {
+                   recursivesearchcall(res.paging.cursors.after);
+               }
+            }
+        }
+            
+        }
     
     getFbList(0); //First Call
 
-function recursivesearchcall(res)
+
+}); 
+}
+
+getRestaurantNames(searchFor);
+
+function recursivesearchcall(aftertoken)
 {
-     if(res.paging && res.paging.next)
+    var nextsearch = { q: 'restaurant',type: 'place', center: '51.50,-0.10', distance: 100000, limit:500,after: aftertoken};
+     if(aftertoken != "")
       {
-          graph.get(res.paging.next, function(nerr, nres) {
-              if(nerr)
-              {
-                  console.log(nerr);
-              }
-              var data = nres.data;
-              
-              function getnextFbList(j)
-                {
-                    if(j < data.length)
-                    {
-                        connection.query("SELECT * FROM restaurantlist WHERE restaurantid ='" + data[j].id+ "'",function(nferr,nfrows,nffields)
-                               {
-                              //console.log("INSERT INTO restaurantlist (restaurantid,restaurantnames) values('" + obj[i].id + "','" +obj[i].name.replace(/'/,'') + "')");
-                                   if(nfrows.length == 0)
-                                   {
-                                   	connection.query("INSERT INTO restaurantlist (restaurantid,restaurantnames) values('" + data[j].id + "','" +data[j].name.replace(/'/g,'') + "')", function(err, rows, fields) {
-                                			if (err){
-                                				console.log(err);
-                                			}
-                                			
-                                			console.log("Row inserted");
-                                			  getnextFbList(j + 1);
-                                		});
-                                   }
-                               });
-                      
-                    }
-                }
-   
-                getnextFbList(0);  //Call next page link
-                
-                if(nres.paging && nres.paging.next)
-                {
-                  recursivesearchcall(nres);  
-                }
-                
-            });
+         getRestaurantNames(nextsearch);
               
       }
               
 }
-
-}); 
 
 
