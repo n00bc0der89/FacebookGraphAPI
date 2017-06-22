7f7600bb2ca83242c4d34ef1e812c7753c1f5d5a
Sachin Shetty <scn.3089@gmail.com>
2017-06-22 10:54:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports.constants = {
 	mysql_database	: 'c9',
 	mysql_host		: '0.0.0.0',
 	mysql_port      : "3306",
-	facebook_app_id :   "191150784642113",
-	facebook_app_secret:  "6e4d1eb39a683d482f43cf14053589a3",
-	access_token: "EAACEdEose0cBABPIwAyx07CQkeQuwzWrORR1Iug0LGPs8nC8h8jSzHw7dJIa5T253cQRTiGDONsIY5MhGm803w9z3ZAzauZAaHWchNOF1MRzsDxZAOUKptw5IUplJxcLUzApCYYkXBCFke0tmAo2UPCCZBLls69ai8HD64qPWgZDZD"
+	facebook_app_id :   "xxxx",
+	facebook_app_secret:  "xxx",
+	access_token: "xxx"
 }
 
